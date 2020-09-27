61f8fbcd4166eef2ae2d042bbf70bb8799223d20
hasanaliarikannn <hasanaliarikannn>
2020-09-27 14:24:31 +0300

    first commit

---
+++ hava durumu/request.js
@@ -0,0 +1,15 @@
+const key = '242779155895d126645cdf9cbb167c73'
+
+
+const requestCity = async(city) => {
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}`;
+
+    //make fetch call (promise call)
+    const response = await fetch(baseURL + query);
+
+    //promise data
+    const data = await response.json();
+    return data;
+
+}
